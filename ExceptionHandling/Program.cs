2eaab787cf47ddc9dee9d235550b18ae21d12ca8
FIRST_NAME <billuramya2002@gamil.com>
2024-03-13 10:29:22 +0530

    ExceptionHandling

---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class MYExceptions : Exception
+{
+    public override string Message
+    {
+        get
+        {
+            return " iam from myexception class";
+        }
+    }
+}
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        try {
+            int a, b;
+            Console.WriteLine("enter the a value");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter the b value");
+            b = int.Parse(Console.ReadLine());
+            
+            if (b==0)
+            {
+                throw new MYExceptions();
+            }
+        }
+        catch(MYExceptions ex) {
+            Console.WriteLine(ex.Message);
+        }
+        finally 
+        {
+            Console.WriteLine("this is final");
+        }
+}
+}
